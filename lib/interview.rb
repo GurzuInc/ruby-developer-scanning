daeeec34ccb7b8cc5ddf33d2d94a0dbadafdb5a9
kapil <kapilnakhwa@gmail.com>
2015-07-14 20:38:30 +0545

    colorized STDOUT and some eye candies

--- lib/interview.rb
+++ lib/interview.rb
@@ -12,8 +12,8 @@ class Interview
 
   def start
     @questions.each {|question| 
-      say "\n**Here is your question: (Type the answer into the console and press enter when done)**\n\n".red 
-      question.answer = ask(question.text.blue)
+      say "\n**Here is your question: (Type the answer into the console. Type 'submit' and press enter  in new line to submit your answer for the question)**\n\n".red 
+      question.answer = ask(question.text.blue) do |q| q.gather="submit" end
     }
   end
 end
--- lib/interview_cli.rb
+++ lib/interview_cli.rb
@@ -2,8 +2,8 @@ class InterviewCLI < Thor
   desc "start", "start the interview"
   def start    
     # ASK FOR NAME AND EMAIL ADDRESS.
-    name = ask "\nEnter your full name.".green
-    email = ask "\nEnter your email address to get back to you. ".green
+    name = ask "\nEnter your full name.".green 
+    email = ask( "\nEnter your email address to get back to you. ".green) { |q| q.echo = '*' }
     user = User.create(name,email)
     interview = Interview.new(user)
     interview.start
